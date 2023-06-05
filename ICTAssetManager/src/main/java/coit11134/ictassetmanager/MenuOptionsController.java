9917f8d678c06004970c67e8caf87c616034d1a2
Wesley Lwin <133333847+kid-name-finger@users.noreply.github.com>
2023-06-05 14:19:08 +1000

    Update MenuOptionsController.java

--- ICTAssetManager/src/main/java/coit11134/ictassetmanager/MenuOptionsController.java
+++ ICTAssetManager/src/main/java/coit11134/ictassetmanager/MenuOptionsController.java
@@ -1,1 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coit11134.ictassetmanager;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+/**
+ *
+ * @author Wesley
+ */
+public class MenuOptionsController implements Initializable {
+
+}
