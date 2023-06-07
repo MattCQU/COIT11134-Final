576789ff02d028ad054b1dc19e6912ad6f8e0728
serajeong <serenazzang@gmail.com>
2023-06-07 18:47:50 +1000

    creating missing GUI scene (for location information)

--- ICTAssetManager/src/main/java/coit11134/ictassetmanager/AssetManagementStaff.java
+++ ICTAssetManager/src/main/java/coit11134/ictassetmanager/AssetManagementStaff.java
@@ -1,7 +1,7 @@
 /*
 *COIT11134 Assessment 3 Part B
 *
-*Authers: Sera Jeong 12211242, Aye Chan Ko KO LWIN12206477, Matthew Meintjes S0270867
+*Authors: Sera Jeong 12211242, Aye Chan Ko KO LWIN12206477, Matthew Meintjes S0270867
 */
 
 package coit11134.ictassetmanager;
