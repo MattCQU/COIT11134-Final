527704183f6ca88b1ae5eda9fcda9d77bab9f55d
serajeong <serenazzang@gmail.com>
2023-06-07 19:04:15 +1000

    minor editing for staff information page controller

--- ICTAssetManager/src/main/java/coit11134/ictassetmanager/StaffInformationPageController.java
+++ ICTAssetManager/src/main/java/coit11134/ictassetmanager/StaffInformationPageController.java
@@ -6,6 +6,46 @@
 
 package coit11134.ictassetmanager;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class StaffInformationPageController {
+   
     
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnCreate;
+
+    @FXML
+    private Button btnGenerateReport;
+
+    @FXML
+    private Button btnRead;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private ListView<?> listViewAssets;
+
+    @FXML
+    private TextField txtfieldSearch;
+    
+    @FXML
+    private void handleButtonExitAction (ActionEvent event) throws Exception  {
+        System.out.println("You have pressed the Cancel button!");
+        
+        try {
+            App.setRoot("menu");
+        } catch (IOException e){
+           System.out.println(e); 
+        }
+    }
+
 }
